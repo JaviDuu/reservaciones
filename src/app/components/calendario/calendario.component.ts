217dc8caac5c54743c9bcca359b0d74d04a9612c
Luis Jimenez <luis.jimenez.cortes@outlook.com>
2023-05-12 10:47:44 -0600

    Arreglado fechas
    Ya no me sale error con la validcaion de fechas, pero ya no sale el forms

--- src/app/components/calendario/calendario.component.ts
+++ src/app/components/calendario/calendario.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,18 +8,31 @@ import { Router } from '@angular/router';
   templateUrl: './calendario.component.html',
   styleUrls: ['./calendario.component.css']
 })
-export class CalendarioComponent {
+export class CalendarioComponent implements OnInit{
+  public fechaMinima:Date = new Date();
+  public fechaStrMinima: string | null = "";
 
-  calanderForm = this.formBuilder.group({
+  calanderForm: FormGroup = this.formBuilder.group({
     dia: ''
   });
 
   constructor(
     private formBuilder: FormBuilder,
-    private router: Router
-  ) {}
+    private router: Router,
+    private pd: DatePipe
+  ) {
+    this.calanderForm = new FormGroup({
+      'dia': new FormControl('', Validators.required)
+    })
+
+  }
 
   onSubmit(): void{
     this.router.navigate(['reservar/opciones', this.calanderForm.value.dia]);
+  }
+
+  ngOnInit(): void {
+    this.fechaMinima = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+    this.fechaStrMinima = this.pd.transform(this.fechaMinima, "yyyy-MM-dd");
   }
 }
